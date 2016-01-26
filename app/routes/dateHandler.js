65012242dece524cdc230f50759e0bda2c2dac2a
Brad Hale <bradhale007@aol.com>
2016-01-26 03:27:49 +0000

    stack complete, only styling needed now

--- app/routes/dateHandler.js
+++ app/routes/dateHandler.js
@@ -4,6 +4,12 @@ function dateHandler (str) {
     var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     return {'unix': date.getTime(), 'natural': months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()};
   }
+  else {
+    date = new Date(parseInt(decodeURI(str)));
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return {'unix': date.getTime(), 'natural': months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()};
+  }
 }
+
 
 module.exports = dateHandler;
