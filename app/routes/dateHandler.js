416b848a7d1f9bd35213c069cbb4ccdbf630e915
Brad Hale <bradhale007@aol.com>
2016-01-26 03:08:37 +0000

    added module to convert date

---
+++ app/routes/dateHandler.js
@@ -0,0 +1,9 @@
+function dateHandler (str) {
+  var date = new Date(decodeURI(str));
+  if (date.getTime()){
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return {'unix': date.getTime(), 'natural': months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()};
+  }
+}
+
+module.exports = dateHandler;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,18 @@
 'use strict';
 
+var DateHandler = require(process.cwd() + '/app/routes/dateHandler.js');
+
 module.exports = function (app) {
+  var dateHandler = new DateHandler();
   app.route('/')
     .get(function(req, res) {
         res.sendFile(process.cwd() + '/public/index.html');
       });
   app.route('/:stamp')
     .get(function(req, res) {
-      res.send(req.params.stamp);
-    });
+      //var stamp = decodeURI(req.params.stamp);
+      var dateHandler = new DateHandler(req.params.stamp);
+      //var date = new Date(stamp);
+      res.send(JSON.stringify(dateHandler)); });
 };
 
